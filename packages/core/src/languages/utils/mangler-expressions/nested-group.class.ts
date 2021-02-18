75918bf17d70cc7891c5fc21ba14c41f5cf33b32
Eric Cornelissen <ericornelissen@gmail.com>
2021-02-18 17:25:47 +0100

    perf(core): improve CSS class mangling performance in HTML
    Create a new ManglerExpression implementation, used by the CSS class
    mangler's HTML language support, that works in two stages. For CSS
    classes, it first finds any class attribute and fetches its value iff
    the value matches the pattern of interest at least once. Then, it
    matches the value substring against the pattern of interest and returns
    or replaces all instances of the pattern in the substring.

---
+++ packages/core/src/languages/utils/mangler-expressions/nested-group.class.ts
@@ -0,0 +1,138 @@
+import type { ManglerExpression } from "../../../types";
+
+import { format as printf } from "util";
+
+/**
+ * Type of a the groups object of a Regular Expression match.
+ */
+type RegExpMatchGroups = { [key: string]: string };
+
+/**
+ * A {@link NestedGroupExpression} is a {@link ManglerExpression} implementation
+ * that matches and replaces in one-level nested substrings.
+ *
+ * @example
+ * new NestedGroupExpression(
+ *   "(?<=')(?<GROUP_NAME>%s)(?=')",
+ *   "(?<=\s)([a-z])(?=\s)"
+ *   "GROUP_NAME",
+ * );
+ * // matches "horse" & "battery" in "var pw = 'correct horse battery staple';"
+ * // and for the replacements "horse->zebra" and "battery->cell" will change it
+ * // into "var pw = 'correct zebra cell staple';"
+ *
+ * @since v0.1.12
+ */
+export default class NestedGroupExpression implements ManglerExpression {
+  /**
+   * The top-level template string to use as (generic) pattern to find
+   * substrings in the target string that can be processed by the
+   * `subPatternTemplate`.
+   */
+  private readonly superPatternTemplate: string;
+
+  /**
+   * The sub template string to use as (generic) pattern against substrings
+   * produced by `superPatternTemplate`.
+   */
+  private readonly subPatternTemplate: string;
+
+  /**
+   * The name of the group in `superPatternTemplate` and `subPatternTemplate` to
+   * match and replace.
+   */
+  private readonly groupName: string;
+
+  /**
+   *
+   * NOTE 1: whitespace is automatically removed from both templates.
+   * NOTE 2: the class assumes the provided group is present in both templates.
+   *
+   * @param superPatternTemplate The top-level template.
+   * @param subPatternTemplate The sub template.
+   * @param groupName The name of a group in both pattern templates.
+   * @since v0.1.12
+   */
+  constructor(
+    superPatternTemplate: string,
+    subPatternTemplate: string,
+    groupName: string,
+  ) {
+    this.superPatternTemplate = superPatternTemplate.replace(/\s/g, "");
+    this.subPatternTemplate = subPatternTemplate.replace(/\s/g, "");
+    this.groupName = groupName;
+  }
+
+  /**
+   * @inheritdoc
+   * @since v0.1.12
+   */
+  public * exec(s: string, pattern: string): IterableIterator<string> {
+    const regExp = this.newRegExp(this.superPatternTemplate, pattern);
+    let match: RegExpExecArray | null = null;
+    while ((match = regExp.exec(s)) !== null) {
+      const groups = match.groups as RegExpMatchGroups;
+      const subStr = groups[this.groupName];
+
+      const regExpSub = this.newRegExp(this.subPatternTemplate, pattern);
+      let matchSub: RegExpExecArray | null = null;
+      while ((matchSub = regExpSub.exec(subStr)) !== null) {
+        const subGroups = matchSub.groups as RegExpMatchGroups;
+        yield subGroups[this.groupName];
+      }
+    }
+  }
+
+  /**
+   * NOTE: not implemented as this function is deprecated.
+   *
+   * @inheritdoc
+   * @throws Always.
+   * @since v0.1.12
+   * @deprecated
+   */
+  public replace(): string {
+    throw new Error("Not implemented");
+  }
+
+  /**
+   * @inheritdoc
+   * @since v0.1.12
+   */
+  public replaceAll(
+    s: string,
+    replacements: Map<string, string>,
+  ): string {
+    if (replacements.size === 0) {
+      return s;
+    }
+
+    const pattern = Array.from(replacements.keys()).join("|");
+    const regExp = this.newRegExp(this.superPatternTemplate, pattern);
+    const regExpSub = this.newRegExp(this.subPatternTemplate, pattern);
+    return s.replace(regExp, (...args: unknown[]): string => {
+      const groups = args[args.length - 1] as RegExpMatchGroups;
+      const subStr = groups[this.groupName];
+
+      return subStr.replace(regExpSub, (...args: unknown[]): string => {
+        const groups = args[args.length - 1] as RegExpMatchGroups;
+        const original = groups[this.groupName];
+        const replacement = replacements.get(original);
+        return replacement as string;
+      });
+    });
+  }
+
+  /**
+   * Create a new {@link RegExp} from a given `patternTemplate` and a given
+   * string or pattern.
+   *
+   * @param template The regular expression template.
+   * @param pattern The string or pattern of interest.
+   * @returns A {@link RegExp} corresponding to the `template` and `pattern`.
+   */
+  private newRegExp(template: string, pattern: string): RegExp {
+    const rawExpr = printf(template, pattern);
+    return new RegExp(rawExpr, "gm");
+  }
+}
