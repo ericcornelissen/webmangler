2fd4769ac3d3e856b0cdef45207a2d8078aefa0c
Eric Cornelissen <ericornelissen@gmail.com>
2021-03-12 19:47:55 +0100

    feat(testing): add mocks for three more WebMangler interfaces
    Add three more mocks to the testing utilities package for the
    MangleExpression interface, the WebManglerLanguagePlugin interface, and
    the WebManglerPlugin interface. The latter two are based on the existing
    mocks present in package/core. The former isn't, as the existing mock
    contains more logic than is really necessary, instead this mock is
    created in the same style as the latter two.
    
    These mocks use SinonJS [1] in their implementation, hence Sinon is
    added as a peerDependency to the package/testing/package.json.
    
    --
    1. https://sinonjs.org/

--- packages/testing/src/mocks/index.ts
+++ packages/testing/src/mocks/index.ts
@@ -1,1 +1,4 @@
+export { default as MangleExpressionMock } from "./mangle-expression";
 export { default as WebManglerFileMock } from "./web-mangler-file";
+export { default as WebManglerPluginMock } from "./web-mangler-plugin";
+export { default as WebManglerPluginLanguageMock } from "./web-mangler-language-plugin";
