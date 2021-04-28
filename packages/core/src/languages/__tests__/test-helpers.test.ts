592a3266d061078402fd58317cac05eb58fc7784
Eric Cornelissen <ericornelissen@gmail.com>
2021-04-28 23:49:19 +0200

    chore(core): remove `matchesAsExpected` helper

--- packages/core/src/languages/__tests__/test-helpers.test.ts
+++ packages/core/src/languages/__tests__/test-helpers.test.ts
@@ -5,7 +5,7 @@ import { MangleExpressionMock } from "@webmangler/testing";
 import { expect } from "chai";
 import * as sinon from "sinon";
 
-import { getAllMatches, matchesAsExpected } from "./test-helpers";
+import { getAllMatches } from "./test-helpers";
 
 suite("Language Plugins Test Helpers", function() {
   suite("::getAllMatches", function() {
@@ -74,96 +74,6 @@ suite("Language Plugins Test Helpers", function() {
 
           const matches = getAllMatches(expressions, input, pattern);
           expect(matches).to.deep.equal(expected);
-        }
-      });
-    }
-  });
-
-  suite("::matchesAsExpected", function() {
-    type TestCase = {
-      /**
-       * The {@link MangleExpression}s to match with.
-       */
-      expressions: MangleExpression[];
-
-      /**
-       * The input string to match against.
-       */
-      input: string;
-
-      /**
-       * The pattern to use for matching.
-       */
-      pattern: string;
-
-      /**
-       * The expected matches.
-       */
-      expected: string[];
-
-      /**
-       * The expected result of {@link matchesAsExpected}.
-       */
-      expectedResult: boolean;
-    };
-
-    const scenarios: TestScenario<TestCase>[] = [
-      {
-        name: "sample",
-        cases: [
-          {
-            expressions: [
-              new MangleExpressionMock(
-                sinon.stub().returns(["the", "sun"]),
-              ),
-            ],
-            input: "praise the sun",
-            pattern: "[a-z]",
-            expected: ["the", "sun"],
-            expectedResult: true,
-          },
-          {
-            expressions: [
-              new MangleExpressionMock(
-                sinon.stub().returns(["the", "sun"]),
-              ),
-            ],
-            input: "praise the sun",
-            pattern: "[a-z]",
-            expected: ["praise"],
-            expectedResult: false,
-          },
-          {
-            expressions: [
-              new MangleExpressionMock(
-                sinon.stub().returns(["the", "sun"]),
-              ),
-              new MangleExpressionMock(
-                sinon.stub().returns(["ise", "the"]),
-              ),
-            ],
-            input: "praise the sun",
-            pattern: "[a-z]",
-            expected: ["ise", "the"],
-            expectedResult: true,
-          },
-        ],
-      },
-    ];
-
-    for (const { name, cases } of scenarios) {
-      test(name, function() {
-        for (const testCase of cases) {
-          const {
-            expressions,
-            input,
-            pattern,
-            expected,
-            expectedResult,
-          } = testCase;
-
-          const res = matchesAsExpected(expressions, input, pattern, expected);
-          expect(res).to.equal(expectedResult);
         }
       });
     }
--- packages/core/src/languages/__tests__/test-helpers.ts
+++ packages/core/src/languages/__tests__/test-helpers.ts
@@ -23,31 +23,3 @@ export function getAllMatches(
 
   return matches;
 }
-
-/**
- * Check whether any of the `expression`s matches the `expected` values on the
- * given `input` with the given `pattern`.
- *
- * @param expressions The {@link MangleExpression}s to test with.
- * @param input The input string to test on.
- * @param pattern The pattern to test with.
- * @param expected The expected matches.
- * @returns `true` if any of the expressions matches as expected.
- */
-export function matchesAsExpected(
-  expressions: Iterable<MangleExpression>,
-  input: string,
-  pattern: string,
-  expected: string[],
-): boolean {
-  return Array.from(expressions).some((expression) => {
-    let i = 0;
-    for (const match of expression.exec(input, pattern)) {
-      if (match !== expected[i++]) {
-        return false;
-      }
-    }
-
-    return i === expected.length;
-  });
-}
