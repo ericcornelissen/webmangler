867f2bf6b65e65b818b57211ad8fd6cda76ca162
Eric Cornelissen <ericornelissen@gmail.com>
2021-04-28 23:22:19 +0200

    test(core): add new helper for testing expression factories
    Add a new test helper for the core's language plugin's expression
    factories. The existing helper is unusable when a test fails as it can
    only tell you: "the test failed on this input". With this new helper the
    test can fail with a comparison between the expected matches an the
    actual matches, as the helper returns the actual matches.

--- packages/core/src/languages/__tests__/test-helpers.test.ts
+++ packages/core/src/languages/__tests__/test-helpers.test.ts
@@ -5,94 +5,167 @@ import { MangleExpressionMock } from "@webmangler/testing";
 import { expect } from "chai";
 import * as sinon from "sinon";
 
-import { matchesAsExpected } from "./test-helpers";
+import { getAllMatches, matchesAsExpected } from "./test-helpers";
 
-suite("::matchesAsExpected", function() {
-  type TestCase = {
-    /**
-     * The {@link MangleExpression}s to match with.
-     */
-    expressions: MangleExpression[];
+suite("Language Plugins Test Helpers", function() {
+  suite("::getAllMatches", function() {
+    type TestCase = {
+      /**
+       * The {@link MangleExpression}s to match with.
+       */
+      expressions: MangleExpression[];
 
-    /**
-     * The input string to match against.
-     */
-    input: string;
+      /**
+       * The input string to match against.
+       */
+      input: string;
 
-    /**
-     * The pattern to use for matching.
-     */
-    pattern: string;
+      /**
+       * The pattern to use for matching.
+       */
+      pattern: string;
 
-    /**
-     * The expected matches.
-     */
-    expected: string[];
+      /**
+       * The expected matches.
+       */
+      expected: string[];
+    };
 
-    /**
-     * The expected result of {@link matchesAsExpected}.
-     */
-    expectedResult: boolean;
-  };
+    const scenarios: TestScenario<TestCase>[] = [
+      {
+        name: "sample",
+        cases: [
+          {
+            expressions: [
+              new MangleExpressionMock(
+                sinon.stub().returns(["the", "sun"]),
+              ),
+            ],
+            input: "praise the sun",
+            pattern: "[a-z]",
+            expected: ["the", "sun"],
+          },
+          {
+            expressions: [
+              new MangleExpressionMock(
+                sinon.stub().returns(["the", "sun"]),
+              ),
+              new MangleExpressionMock(
+                sinon.stub().returns(["ise", "the"]),
+              ),
+            ],
+            input: "praise the sun",
+            pattern: "[a-z]",
+            expected: ["the", "sun", "ise", "the"],
+          },
+        ],
+      },
+    ];
 
-  const scenarios: TestScenario<TestCase>[] = [
-    {
-      name: "sample",
-      cases: [
-        {
-          expressions: [
-            new MangleExpressionMock(
-              sinon.stub().returns(["the", "sun"]),
-            ),
-          ],
-          input: "praise the sun",
-          pattern: "[a-z]",
-          expected: ["the", "sun"],
-          expectedResult: true,
-        },
-        {
-          expressions: [
-            new MangleExpressionMock(
-              sinon.stub().returns(["the", "sun"]),
-            ),
-          ],
-          input: "praise the sun",
-          pattern: "[a-z]",
-          expected: ["praise"],
-          expectedResult: false,
-        },
-        {
-          expressions: [
-            new MangleExpressionMock(
-              sinon.stub().returns(["the", "sun"]),
-            ),
-            new MangleExpressionMock(
-              sinon.stub().returns(["ise", "the"]),
-            ),
-          ],
-          input: "praise the sun",
-          pattern: "[a-z]",
-          expected: ["ise", "the"],
-          expectedResult: true,
-        },
-      ],
-    },
-  ];
+    for (const { name, cases } of scenarios) {
+      test(name, function() {
+        for (const testCase of cases) {
+          const {
+            expressions,
+            input,
+            pattern,
+            expected,
+          } = testCase;
 
-  for (const { name, cases } of scenarios) {
-    test(name, function() {
-      for (const testCase of cases) {
-        const {
-          expressions,
-          input,
-          pattern,
-          expected,
-          expectedResult,
-        } = testCase;
+          const matches = getAllMatches(expressions, input, pattern);
+          expect(matches).to.deep.equal(expected);
+        }
+      });
+    }
+  });
 
-        const result = matchesAsExpected(expressions, input, pattern, expected);
-        expect(result).to.equal(expectedResult);
-      }
-    });
-  }
+  suite("::matchesAsExpected", function() {
+    type TestCase = {
+      /**
+       * The {@link MangleExpression}s to match with.
+       */
+      expressions: MangleExpression[];
+
+      /**
+       * The input string to match against.
+       */
+      input: string;
+
+      /**
+       * The pattern to use for matching.
+       */
+      pattern: string;
+
+      /**
+       * The expected matches.
+       */
+      expected: string[];
+
+      /**
+       * The expected result of {@link matchesAsExpected}.
+       */
+      expectedResult: boolean;
+    };
+
+    const scenarios: TestScenario<TestCase>[] = [
+      {
+        name: "sample",
+        cases: [
+          {
+            expressions: [
+              new MangleExpressionMock(
+                sinon.stub().returns(["the", "sun"]),
+              ),
+            ],
+            input: "praise the sun",
+            pattern: "[a-z]",
+            expected: ["the", "sun"],
+            expectedResult: true,
+          },
+          {
+            expressions: [
+              new MangleExpressionMock(
+                sinon.stub().returns(["the", "sun"]),
+              ),
+            ],
+            input: "praise the sun",
+            pattern: "[a-z]",
+            expected: ["praise"],
+            expectedResult: false,
+          },
+          {
+            expressions: [
+              new MangleExpressionMock(
+                sinon.stub().returns(["the", "sun"]),
+              ),
+              new MangleExpressionMock(
+                sinon.stub().returns(["ise", "the"]),
+              ),
+            ],
+            input: "praise the sun",
+            pattern: "[a-z]",
+            expected: ["ise", "the"],
+            expectedResult: true,
+          },
+        ],
+      },
+    ];
+
+    for (const { name, cases } of scenarios) {
+      test(name, function() {
+        for (const testCase of cases) {
+          const {
+            expressions,
+            input,
+            pattern,
+            expected,
+            expectedResult,
+          } = testCase;
+
+          const res = matchesAsExpected(expressions, input, pattern, expected);
+          expect(res).to.equal(expectedResult);
+        }
+      });
+    }
+  });
 });
