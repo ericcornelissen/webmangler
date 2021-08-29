7d6723c084d90a84ce4551ec24ac0df5371bd0e6
Eric Cornelissen <ericornelissen@gmail.com>
2021-08-29 17:32:39 +0200

    test(lang-css): add initial unit test suites

---
+++ packages/language-css/src/expressions/__tests__/unit/css-values.test.ts
@@ -0,0 +1,29 @@
+import { expect } from "chai";
+
+import expressionsFactory from "../../css-values";
+
+suite("CSS Values Expression Factory", function() {
+  test("no prefix, no suffix", function() {
+    const _result = expressionsFactory({ });
+    const result = Array.from(_result);
+    expect(result.length).to.be.greaterThan(0);
+  });
+
+  test("with prefix, on suffix", function() {
+    const _result = expressionsFactory({ prefix: "foobar" });
+    const result = Array.from(_result);
+    expect(result.length).to.be.greaterThan(0);
+  });
+
+  test("no prefix, with suffix", function() {
+    const _result = expressionsFactory({ suffix: "foobar" });
+    const result = Array.from(_result);
+    expect(result.length).to.be.greaterThan(0);
+  });
+
+  test("with prefix, with suffix", function() {
+    const _result = expressionsFactory({ prefix: "foo", suffix: "bar" });
+    const result = Array.from(_result);
+    expect(result.length).to.be.greaterThan(0);
+  });
+});
