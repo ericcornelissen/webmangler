447b757a80e29aae4917aa409d2ecbdad66116c0
Eric Cornelissen <ericornelissen@gmail.com>
2021-09-27 23:42:03 +0200

    refactor(mangler-html-attributes): extract helpers from main class

---
+++ packages/mangler-html-attributes/src/helpers/index.ts
@@ -0,0 +1,18 @@
+import {
+  getAttributeExpressionOptions,
+  getAttributeSelectorExpressionOptions,
+  getAttributeUsageExpressionFactory,
+} from "./language-options";
+import { getIgnorePatterns, getPatterns } from "./patterns";
+import { getPrefix } from "./prefix";
+import { getReserved } from "./reserved";
+
+export {
+  getAttributeExpressionOptions,
+  getAttributeSelectorExpressionOptions,
+  getAttributeUsageExpressionFactory,
+  getIgnorePatterns,
+  getPatterns,
+  getPrefix,
+  getReserved,
+};
