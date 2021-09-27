999cfaef95eea436fc1b9aac1a747e58fd8492ab
Eric Cornelissen <ericornelissen@gmail.com>
2021-09-27 23:10:32 +0200

    ci: generate different job matrices for different jobs
    Update the changes job of the push-checks workflow to generate job
    matricies per job. In particular, the job matrix for the test job only
    includes packages that have tests and the matrix for the benchmark job
    only includes packages that have benchmark tests.

--- scripts/ci/get-filters.js
+++ scripts/ci/get-filters.js
@@ -1,10 +1,64 @@
+#!/usr/bin/env node
+/**
+ * @fileoverview
+ * Generate a `filters` YAML string for dorny/paths-filter for the various
+ * conditional jobs (first argument). The filters are based on what files are
+ * present in each package - e.g. a package without benchmarks isn't included
+ * in the output for "benchmark".
+ */
+
+import * as fs from "fs";
+
 import log from "../utilities/log.js";
 import * as paths from "../paths.js";
 
-const filters = [];
-paths.getPackages().forEach((packageName) => {
-  filters.push(`${packageName}: packages/${packageName}/src/**`);
-});
+main(process.argv);
 
-const filtersRaw = filters.join("\n");
-log.print(filtersRaw);
+function main(argv) {
+  const packageCriteria = getPackageCriteria(argv[2]);
+  const filters = getPackageFilters(packageCriteria);
+  log.print(filters);
+}
+
+function getPackageCriteria(arg) {
+  switch (arg) {
+    case "benchmark":
+      return (pkg) => hasFiles(pkg, /\.bench\.ts$/);
+    case "test":
+      return (pkg) => hasFiles(pkg, /\.test\.ts$/);
+    default:
+      return () => true;
+  }
+}
+
+function getPackageFilters(packageCriteria) {
+  const filters = paths.getPackages()
+    .filter(packageCriteria)
+    .map(asPackageFilter)
+    .join("\n");
+  return filters;
+}
+
+function asPackageFilter(packageName) {
+  return `${packageName}: packages/${packageName}/src/**`;
+}
+
+function hasFiles(pkg, fileRegExp) {
+  const helper = (folder) => {
+    for (const entry of fs.readdirSync(folder)) {
+      const entryPath = paths.resolve._(folder, entry);
+      const stats = fs.statSync(entryPath);
+      if (!stats.isFile()) {
+        const result = helper(entryPath);
+        if (result) return result;
+      } else {
+        const result = fileRegExp.test(entryPath);
+        if (result) return result;
+      }
+    }
+    return false;
+  };
+
+  const packageRoot = paths.resolve.fromPackage(pkg, "src");
+  return helper(packageRoot);
+}
