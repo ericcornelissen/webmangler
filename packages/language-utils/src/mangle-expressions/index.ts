b8babf26776eb1214582940d7e468cee25a2dcba
Eric Cornelissen <ericornelissen@gmail.com>
2021-12-16 19:52:20 +0100

    refactor(language-utils): use separate export statements everywhere

--- packages/language-utils/src/mangle-expressions/index.ts
+++ packages/language-utils/src/mangle-expressions/index.ts
@@ -1,2 +1,7 @@
-export { default as NestedGroupMangleExpression } from "./nested-group.class";
-export { default as SingleGroupMangleExpression } from "./single-group.class";
+import NestedGroupMangleExpression from "./nested-group.class";
+import SingleGroupMangleExpression from "./single-group.class";
+
+export {
+  NestedGroupMangleExpression,
+  SingleGroupMangleExpression,
+};
