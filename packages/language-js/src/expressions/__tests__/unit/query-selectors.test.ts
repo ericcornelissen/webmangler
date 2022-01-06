6f3709aa6fc3022fc120b461f6a3ae4589344b4f
Eric Cornelissen <ericornelissen@gmail.com>
2022-01-06 10:47:32 +0100

    test(language-js): improve tests based on mutation test results
    - Add tests to verify the contents of the `QUOTES_ARRAY` common value.
    - Improve tests for query-selector expression factory. Verify the
      conditional expressions are only present in certain scenarios.

--- packages/language-js/src/expressions/__tests__/unit/query-selectors.test.ts
+++ packages/language-js/src/expressions/__tests__/unit/query-selectors.test.ts
@@ -10,20 +10,32 @@ suite("Query Selector Expression Factory", function() {
   });
 
   test("with prefix, on suffix", function() {
+    const _base = expressionsFactory({ });
+    const base = Array.from(_base);
+
     const _result = expressionsFactory({ prefix: "foobar" });
     const result = Array.from(_result);
     expect(result.length).to.be.greaterThan(0);
+    expect(result.length).to.be.greaterThan(base.length);
   });
 
   test("no prefix, with suffix", function() {
+    const _base = expressionsFactory({ });
+    const base = Array.from(_base);
+
     const _result = expressionsFactory({ suffix: "foobar" });
     const result = Array.from(_result);
     expect(result.length).to.be.greaterThan(0);
+    expect(result.length).to.be.greaterThan(base.length);
   });
 
   test("with prefix, with suffix", function() {
+    const _base = expressionsFactory({ });
+    const base = Array.from(_base);
+
     const _result = expressionsFactory({ prefix: "foo", suffix: "bar" });
     const result = Array.from(_result);
     expect(result.length).to.be.greaterThan(0);
+    expect(result.length).to.be.greaterThan(base.length);
   });
 });
