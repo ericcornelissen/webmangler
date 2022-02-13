c32b1c8543342426c7fc1faf2f40fde20877d3bc
Eric Cornelissen <ericornelissen@gmail.com>
2022-02-13 15:14:30 +0100

    refactor(mangler-html-attributes): dependency injection for class

--- packages/mangler-html-attributes/src/__tests__/unit/class.test.ts
+++ packages/mangler-html-attributes/src/__tests__/unit/class.test.ts
@@ -1,20 +1,212 @@
-import { expect } from "chai";
+import type { SinonStub } from "sinon";
 
-import HtmlAttributeMangler from "../../class";
+import type { HtmlAttributeManglerOptions } from "../../types";
+
+import { expect, use as chaiUse } from "chai";
+import * as sinon from "sinon";
+import * as sinonChai from "sinon-chai";
+
+import initHtmlAttributeMangler from "../../class";
+
+chaiUse(sinonChai);
+
+interface TestScenario {
+  readonly suiteName: string;
+  readonly testCase: HtmlAttributeManglerOptions | undefined;
+}
 
 suite("HtmlAttributeMangler class", function() {
-  let subject: HtmlAttributeMangler;
+  let HtmlAttributeMangler: ReturnType<typeof initHtmlAttributeMangler>;
+
+  let getCharacterSet: SinonStub;
+  let getPatterns: SinonStub;
+  let getIgnorePatterns: SinonStub;
+  let getReserved: SinonStub;
+  let getPrefix: SinonStub;
+  let getLanguageOptions: SinonStub;
+
+  suiteSetup(function() {
+    getCharacterSet = sinon.stub();
+    getPatterns = sinon.stub();
+    getIgnorePatterns = sinon.stub();
+    getReserved = sinon.stub();
+    getPrefix = sinon.stub();
+    getLanguageOptions = sinon.stub();
+
+    HtmlAttributeMangler = initHtmlAttributeMangler({
+      getCharacterSet,
+      getPatterns,
+      getIgnorePatterns,
+      getReserved,
+      getPrefix,
+      getLanguageOptions,
+    });
+  });
 
   setup(function() {
-    subject = new HtmlAttributeMangler();
+    getCharacterSet.resetHistory();
+    getPatterns.resetHistory();
+    getIgnorePatterns.resetHistory();
+    getReserved.resetHistory();
+    getPrefix.resetHistory();
+    getLanguageOptions.resetHistory();
   });
 
-  test("instantiation succeeds", function() {
-    expect(subject).not.to.be.undefined;
-  });
+  const testScenarios: Iterable<TestScenario> = [
+    {
+      suiteName: "No config",
+      testCase: undefined,
+    },
+    {
+      suiteName: "Empty configuration object",
+      testCase: { },
+    },
+    {
+      suiteName: "Only patterns",
+      testCase: {
+        attrNamePattern: "foo[a-z]{3}",
+      },
+    },
+    {
+      suiteName: "Only ignore patterns",
+      testCase: {
+        ignoreAttrNamePattern: "[a-z]{3}bar",
+      },
+    },
+    {
+      suiteName: "Only reserved names",
+      testCase: {
+        reservedAttrNames: ["foo", "bar"],
+      },
+    },
+    {
+      suiteName: "Only prefix",
+      testCase: {
+        keepAttrPrefix: "data-",
+      },
+    },
+    {
+      suiteName: "Patterns and ignore patterns",
+      testCase: {
+        attrNamePattern: "foo[a-z]{3}",
+        ignoreAttrNamePattern: "[a-z]{3}bar",
+      },
+    },
+    {
+      suiteName: "Patterns and reserved names",
+      testCase: {
+        attrNamePattern: "foo[a-z]{3}",
+        reservedAttrNames: ["foo", "bar"],
+      },
+    },
+    {
+      suiteName: "Patterns and prefix",
+      testCase: {
+        attrNamePattern: "foo[a-z]{3}",
+        keepAttrPrefix: "data-",
+      },
+    },
+    {
+      suiteName: "Ignore patterns and reserved names",
+      testCase: {
+        ignoreAttrNamePattern: "[a-z]{3}bar",
+        reservedAttrNames: ["foo", "bar"],
+      },
+    },
+    {
+      suiteName: "Ignore patterns and prefix",
+      testCase: {
+        ignoreAttrNamePattern: "[a-z]{3}bar",
+        keepAttrPrefix: "data-",
+      },
+    },
+    {
+      suiteName: "Reserved names and prefix",
+      testCase: {
+        reservedAttrNames: ["foo", "bar"],
+        keepAttrPrefix: "data-",
+      },
+    },
+    {
+      suiteName: "Patterns, ignore patterns, and reserved names",
+      testCase: {
+        attrNamePattern: "foo[a-z]{3}",
+        ignoreAttrNamePattern: "[a-z]{3}bar",
+        reservedAttrNames: ["foo", "bar"],
+      },
+    },
+    {
+      suiteName: "Patterns, ignore patterns, and prefix",
+      testCase: {
+        attrNamePattern: "foo[a-z]{3}",
+        ignoreAttrNamePattern: "[a-z]{3}bar",
+        keepAttrPrefix: "data-",
+      },
+    },
+    {
+      suiteName: "Ignore patterns, reserved names, and prefix",
+      testCase: {
+        ignoreAttrNamePattern: "[a-z]{3}bar",
+        reservedAttrNames: ["foo", "bar"],
+        keepAttrPrefix: "data-",
+      },
+    },
+    {
+      suiteName: "Patterns, ignore patterns, reserved names, and prefix",
+      testCase: {
+        attrNamePattern: "foo[a-z]{3}",
+        ignoreAttrNamePattern: "[a-z]{3}bar",
+        reservedAttrNames: ["foo", "bar"],
+        keepAttrPrefix: "data-",
+      },
+    },
+  ];
 
-  test("options succeeds", function() {
-    const result = subject.options();
-    expect(result).not.to.be.undefined;
-  });
+  for (const { suiteName, testCase } of testScenarios) {
+    suite(suiteName, function() {
+      const options = testCase;
+
+      setup(function() {
+        new HtmlAttributeMangler(options);
+      });
+
+      test("the `getCharacterSet` function is used", function() {
+        expect(getCharacterSet).to.have.callCount(1);
+        expect(getLanguageOptions).to.have.been.calledWithExactly();
+      });
+
+      test("the `getIgnorePatterns` function is used", function() {
+        expect(getIgnorePatterns).to.have.callCount(1);
+        expect(getIgnorePatterns).to.have.been.calledWithExactly(
+          options?.ignoreAttrNamePattern,
+        );
+      });
+
+      test("the `getLanguageOptions` function is used", function() {
+        expect(getLanguageOptions).to.have.callCount(1);
+        expect(getLanguageOptions).to.have.been.calledWithExactly();
+      });
+
+      test("the `getPatterns` function is used", function() {
+        expect(getPatterns).to.have.callCount(1);
+        expect(getPatterns).to.have.been.calledWithExactly(
+          options?.attrNamePattern,
+        );
+      });
+
+      test("the `getPrefix` function is used", function() {
+        expect(getPrefix).to.have.callCount(1);
+        expect(getPrefix).to.have.been.calledWithExactly(
+          options?.keepAttrPrefix,
+        );
+      });
+
+      test("the `getReserved` function is used", function() {
+        expect(getReserved).to.have.callCount(1);
+        expect(getReserved).to.have.been.calledWithExactly(
+          options?.reservedAttrNames,
+        );
+      });
+    });
+  }
 });
--- packages/mangler-html-attributes/src/class.ts
+++ packages/mangler-html-attributes/src/class.ts
@@ -1,132 +1,108 @@
+import type {
+  CharSet,
+  MangleExpressionOptions,
+  WebManglerPlugin,
+} from "@webmangler/types";
+
 import type { HtmlAttributeManglerOptions } from "./types";
 
 import { SimpleManglerPlugin } from "@webmangler/mangler-utils";
 
-import * as helpers from "./helpers";
+/**
+ * The type of the {@link HtmlAttributeMangler} constructor.
+ */
+type HtmlAttributeManglerConstructor = new (
+  options?: HtmlAttributeManglerOptions
+) => WebManglerPlugin;
 
 /**
- * The HTML attribute mangler is a built-in plugin of _WebMangler_ that can be
- * used to mangle HTML attributes, e.g. "data-foo" in `<img data-foo="bar"/>`.
- *
- * This mangler can be configured using the {@link HtmlAttributeManglerOptions}.
- *
- * The simplest way to configure this mangler is by specifying a single pattern
- * of HTML attributes to mangle, e.g. all "data-" attributes:
- *
- * ```javascript
- * new HtmlAttributeMangler({ attrNamePattern: "data-[a-zA-Z-]+" });
- * ```
- *
- * For more fine-grained control or easier-to-read patterns, you can specify
- * multiple patterns. For example, to mangle only the "data-" attributes for
- * names and ages (provided you consistently prefix those attributes with
- * "data-name" and "data-age" resp.), you can use:
- *
- * ```javascript
- * new HtmlAttributeMangler({
- *   attrNamePattern: ["data-name[a-z-]+", "data-age[a-z-]+"],
- * });
- *
- * // Which is equivalent to:
- * new HtmlAttributeMangler({ attrNamePattern: "data-(name|font)[a-z-]+" });
- * ```
- *
- * If you don't specify any patterns the {@link HtmlAttributeMangler.
- * DEFAULT_PATTERNS} will be used, which will mangle all "data-" attributes.
- *
- * ## Examples
- *
- * _The following examples assume the usage of the built-in language plugins._
- *
- * ### HTML
- *
- * Using the default configuration (`new HtmlAttributeMangler()`) on the HTML:
- *
- * ```html
- * <div class="container">
- *   <p class="text">Hello <span data-name="John"></span></p>
- *    <a data-link-type="outgoing" href="https://www.example.com">Visit our website</a>
- * </div>
- * ```
- *
- * Will result in:
- *
- * ```html
- * <div class="container">
- *   <p class="text">Hello <span data-a="John"></span></p>
- *    <a data-b="outgoing" href="https://www.example.com">Visit our website</a>
- * </div>
- * ```
- *
- * If a prefix of "attr-" is used and the name "a" is reserved, the resulting
- * HTML will instead be:
- *
- * ```css
- * <div class="container">
- *   <p class="text">Hello <span attr-b="John"></span></p>
- *    <a attr-c="outgoing" href="https://www.example.com">Visit our website</a>
- * </div>
- * ```
- *
- * ### CSS
- *
- * Using the default configuration (`new HtmlAttributeMangler()`) on the CSS:
- *
- * ```css
- * span[data-name] { }
- * a[data-link-type="outgoing"] { }
- *
- * .data-name { }
- * #data-link-type { }
- * ```
- *
- * Will result in:
- *
- * ```css
- * span[data-a] { }
- * a[data-b="outgoing"] { }
- *
- * .data-name { }
- * #data-link-type { }
- * ```
- *
- * ### JavaScript
- *
- * Using the default configuration (`new HtmlIdMangler()`) on the JavaScript:
- *
- * ```javascript
- * document.querySelector("span[data-name]");
- * document.querySelector("a[data-link-type]");
- * ```
- *
- * Will result in:
- *
- * ```javascript
- * document.querySelector("span[data-a]");
- * document.querySelector("a[data-b]");
- * ```
- *
- * @since v0.1.0
- * @version v0.1.23
+ * The interface defining the dependencies of the {@link HtmlAttributeMangler}
+ * class.
  */
-class HtmlAttributeMangler extends SimpleManglerPlugin {
+interface HtmlAttributeManglerDependencies {
   /**
-   * Instantiate a new {@link HtmlAttributeMangler}.
+   * Get the {@link CharSet} for the {@link HtmlAttributeMangler}.
    *
-   * @param options The {@link HtmlAttributeManglerOptions}.
-   * @since v0.1.0
-   * @version v0.1.23
+   * @returns The {@link CharSet}.
    */
-  constructor(options: HtmlAttributeManglerOptions={}) {
-    super({
-      charSet: helpers.getCharacterSet(),
-      patterns: helpers.getPatterns(options.attrNamePattern),
-      ignorePatterns: helpers.getIgnorePatterns(options.ignoreAttrNamePattern),
-      reserved: helpers.getReserved(options.reservedAttrNames),
-      prefix: helpers.getPrefix(options.keepAttrPrefix),
-      languageOptions: helpers.getLanguageOptions(),
-    });
-  }
+  getCharacterSet(): CharSet;
+
+  /**
+   * Get the ignore patterns for the {@link HtmlAttributeMangler}.
+   *
+   * @param ignoreAttrNamePattern The configured ignore patterns.
+   * @returns The ignore patterns.
+   */
+  getIgnorePatterns(
+    ignoreAttrNamePattern?: string | Iterable<string>,
+  ): string | Iterable<string>;
+
+  /**
+   * Get the language options for the {@link HtmlAttributeMangler}.
+   *
+   * @returns The language options.
+   */
+  getLanguageOptions(): Iterable<MangleExpressionOptions<unknown>>;
+
+  /**
+   * Get the patterns for the {@link HtmlAttributeMangler}.
+   *
+   * @param attrNamePattern The configured patterns.
+   * @returns The patterns.
+   */
+  getPatterns(
+    attrNamePattern?: string | Iterable<string>,
+  ): string | Iterable<string>;
+
+  /**
+   * Get the mangle prefix for the {@link HtmlAttributeMangler}.
+   *
+   * @param keepAttrPrefix The configured prefix.
+   * @returns The mangle prefix.
+   */
+  getPrefix(
+    keepAttrPrefix?: string,
+  ): string;
+
+  /**
+   * Get the reserved names for the {@link HtmlAttributeMangler}.
+   *
+   * @param reservedAttrNames The configured reserved names.
+   * @returns The reserved names.
+   */
+  getReserved(
+    reservedAttrNames?: Iterable<string>,
+  ): Iterable<string>;
 }
 
-export default HtmlAttributeMangler;
+/**
+ * Initialize the {@link HtmlAttributeMangler} class with explicit dependencies.
+ *
+ * @param helpers The dependencies of the class.
+ * @returns The {@link HtmlAttributeMangler} class.
+ */
+function initHtmlAttributeMangler(
+  helpers: HtmlAttributeManglerDependencies,
+): HtmlAttributeManglerConstructor {
+  return class HtmlAttributeMangler extends SimpleManglerPlugin {
+    /**
+     * Instantiate a new {@link HtmlAttributeMangler}.
+     *
+     * @param options The {@link HtmlAttributeManglerOptions}.
+     * @since v0.1.0
+     * @version v0.1.23
+     */
+    constructor(options: HtmlAttributeManglerOptions={}) {
+      super({
+        charSet: helpers.getCharacterSet(),
+        patterns: helpers.getPatterns(options.attrNamePattern),
+        ignorePatterns: helpers.getIgnorePatterns(options.ignoreAttrNamePattern),
+        reserved: helpers.getReserved(options.reservedAttrNames),
+        prefix: helpers.getPrefix(options.keepAttrPrefix),
+        languageOptions: helpers.getLanguageOptions(),
+      });
+    }
+  };
+}
+
+export default initHtmlAttributeMangler;
