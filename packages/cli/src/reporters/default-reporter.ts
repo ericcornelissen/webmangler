af6d687a31c76800482a3b0cad323393d020ff37
Eric Cornelissen <ericornelissen@gmail.com>
2022-03-08 12:45:56 +0100

    refactor(cli): unify the `DefaultReporter` into a class
    A quite drastic refactoring of the `DefaultReporter` implementation such
    that it's implementation is entirely contained within the class
    definition, rather than partly in the class and partly outside as that
    is a more sensible way to write classes.

--- packages/cli/src/reporters/default-reporter.ts
+++ packages/cli/src/reporters/default-reporter.ts
@@ -2,77 +2,8 @@ import type { Reporter, Stats, Writer } from "./types";
 
 import * as chalk from "chalk";
 
-/**
- * Round a number to at most two decimal places.
- *
- * @param x The number of interest.
- * @returns The number rounded.
- */
-function roundToTwoDecimalPlaces(x: number): number {
-  const rounded = Math.round((x + Number.EPSILON) * 100) / 100;
-  return rounded;
-}
-
-/**
- * Convert a percentage as number to a percentage for output.
- *
- * @param percentage The percentage as a number.
- * @returns The percentage string (without %).
- */
-function getDisplayPercentage(percentage: number): string {
-  if (percentage < 0) {
-    if (percentage > -0.01) {
-      return chalk.green("<-0.01%");
-    }
-
-    const roundedPercentage = roundToTwoDecimalPlaces(percentage);
-    return chalk.greenBright(`${roundedPercentage}%`);
-  } else if (percentage > 0) {
-    if (percentage < 0.01) {
-      return chalk.red("<+0.01%");
-    }
-
-    const roundedPercentage = roundToTwoDecimalPlaces(percentage);
-    return chalk.redBright(`+${roundedPercentage}%`);
-  } else {
-    return chalk.gray("0%");
-  }
-}
-
-/**
- * Log statistics about a _WebMangler_ run.
- *
- * @param writer A {@link Writer}.
- * @param stats The _WebMangler_ run statistics.
- */
-function logStats(
-  writer: Writer,
-  stats: Stats,
-): void {
-  const fileCount: number = stats.files.size;
-  const duration: number = roundToTwoDecimalPlaces(stats.duration);
-
-  if (stats.files.size === 0) {
-    writer.write(`Nothing was mangled (${duration} ms)`);
-    return;
-  }
-
-  stats.files.forEach((fileStats, filePath) => {
-    if (fileStats.changed) {
-      const percentage = getDisplayPercentage(fileStats.changePercentage);
-      const reduction = `${fileStats.sizeBefore} -> ${fileStats.sizeAfter}`;
-      writer.write(`${filePath} ${percentage} (${reduction})`);
-    } else {
-      writer.write(`${filePath} [NOT MANGLED]`);
-    }
-  });
-
-  const aggregate = stats.aggregate;
-  const overallPercentage = getDisplayPercentage(aggregate.changePercentage);
-  const overallReduction = `${aggregate.sizeBefore} -> ${aggregate.sizeAfter}`;
-  writer.write(`OVERALL ${overallPercentage} (${overallReduction})`);
-  writer.write(`\nmangled ${fileCount} files in ${duration} ms`);
-}
+type AggregateStats = Stats["aggregate"];
+type FileStats = Stats["aggregate"];
 
 /**
  * The default reporter for the _WebMangler_ CLI.
@@ -96,7 +27,102 @@ class DefaultReporter implements Reporter {
    * @inheritDoc
    */
   async report(stats: Stats): Promise<void> {
-    logStats(this.writer, stats);
+    const fileCount: number = stats.files.size;
+    const duration: number = this.roundToTwoDecimalPlaces(stats.duration);
+
+    if (stats.files.size === 0) {
+      this.writer.write(`Nothing was mangled (${duration} ms)`);
+      return;
+    }
+
+    stats.files.forEach((fileStats: FileStats, filePath: string) => {
+      this.reportFile(fileStats, filePath);
+    });
+
+    this.reportAggregated(stats.aggregate);
+    this.writer.write(`\nmangled ${fileCount} files in ${duration} ms`);
+  }
+
+  /**
+   * Report the aggregated mangle stats.
+   *
+   * @param stats The aggregated stats.
+   */
+  private reportAggregated(stats: AggregateStats): void {
+    const overallPercentage = this.getDisplayPercentage(stats);
+    const overallReduction = this.getNumericChangeString(stats);
+    this.writer.write(`OVERALL ${overallPercentage} (${overallReduction})`);
+  }
+
+  /**
+   * Report the mangle stats for one file.
+   *
+   * @param stats The {@link FileStats}.
+   * @param filepath The path to the file of which the `stats` are.
+   */
+  private reportFile(stats: FileStats, filepath: string): void {
+    if (stats.changed) {
+      const percentage = this.getDisplayPercentage(stats);
+      const reduction = this.getNumericChangeString(stats);
+      this.writer.write(`${filepath} ${percentage} (${reduction})`);
+    } else {
+      this.writer.write(`${filepath} [NOT MANGLED]`);
+    }
+  }
+
+  /**
+   * Create a string representing a numeric change.
+   *
+   * @param params The method parameters.
+   * @param params.sizeAfter The numeric size after.
+   * @param params.sizeBefore The numeric size before.
+   * @returns The numeric change represented as a string.
+   */
+  private getNumericChangeString({ sizeAfter, sizeBefore }: {
+    sizeAfter: number;
+    sizeBefore: number;
+  }): string {
+    return `${sizeBefore} -> ${sizeAfter}`;
+  }
+
+  /**
+   * Convert a percentage as number to a percentage for output.
+   *
+   * @param params The method parameters.
+   * @param params.changePercentage The percentage as a number.
+   * @returns The percentage string (without %).
+   */
+  private getDisplayPercentage({ changePercentage }: {
+    changePercentage: number;
+  }): string {
+    if (changePercentage < 0) {
+      if (changePercentage > -0.01) {
+        return chalk.green("<-0.01%");
+      }
+
+      const roundedPercentage = this.roundToTwoDecimalPlaces(changePercentage);
+      return chalk.greenBright(`${roundedPercentage}%`);
+    } else if (changePercentage > 0) {
+      if (changePercentage < 0.01) {
+        return chalk.red("<+0.01%");
+      }
+
+      const roundedPercentage = this.roundToTwoDecimalPlaces(changePercentage);
+      return chalk.redBright(`+${roundedPercentage}%`);
+    } else {
+      return chalk.gray("0%");
+    }
+  }
+
+  /**
+   * Round a number to at most two decimal places.
+   *
+   * @param x The number of interest.
+   * @returns The number rounded.
+   */
+  private roundToTwoDecimalPlaces(x: number): number {
+    const rounded = Math.round((x + Number.EPSILON) * 100) / 100;
+    return rounded;
   }
 }
 
