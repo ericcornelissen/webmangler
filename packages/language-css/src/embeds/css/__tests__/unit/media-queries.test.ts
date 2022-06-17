0c6ccb853da1cd09222c0800b2fd255b88891a5a
Eric Cornelissen <ericornelissen@gmail.com>
2022-06-17 10:13:34 +0200

    feat(language-css): media query embeds
    Implement & test the detection of media query embeds in CSS content. The
    implementation is modelled after that of embeds detection present in the
    HTML language plugin.

--- packages/language-css/src/embeds/css/__tests__/unit/media-queries.test.ts
+++ packages/language-css/src/embeds/css/__tests__/unit/media-queries.test.ts
@@ -20,13 +20,165 @@ suite("CSS CSS Embeds - Media queries", function() {
           },
           expected: [
             {
-              content: " .foobar{color:red;}",
+              content: " .foobar{color:red;} ",
               type: EMBED_TYPE_CSS,
-              startIndex: 17,
+              startIndex: 15,
               endIndex: 36,
-              getRaw(): string { return " .foobar{color:red;}"; },
+              getRaw(): string { return " .foobar{color:red;} "; },
             },
           ],
+        },
+        {
+          file: {
+            type: "css",
+            content: "@media screen and (max-width: 600px) {#id{color:blue;}}",
+          },
+          expected: [
+            {
+              content: "#id{color:blue;}",
+              type: EMBED_TYPE_CSS,
+              startIndex: 38,
+              endIndex: 54,
+              getRaw(): string { return "#id{color:blue;}"; },
+            },
+          ],
+        },
+        {
+          file: {
+            type: "css",
+            content: ".foo{color:red;}" +
+              "@media print {#id{font:monospace;}}" +
+              ".bar{color:orange;}" +
+              "@media screen {.foo{color:blue;}}",
+          },
+          expected: [
+            {
+              content: "#id{font:monospace;}",
+              type: EMBED_TYPE_CSS,
+              startIndex: 30,
+              endIndex: 50,
+              getRaw(): string { return "#id{font:monospace;}"; },
+            },
+            {
+              content: ".foo{color:blue;}",
+              type: EMBED_TYPE_CSS,
+              startIndex: 85,
+              endIndex: 102,
+              getRaw(): string { return ".foo{color:blue;}"; },
+            },
+          ],
+        },
+      ],
+    },
+    {
+      testName: "comments",
+      getScenario: () => [
+        {
+          file: {
+            type: "css",
+            content: "/*@media screen { .foobar{color:red;} }*/",
+          },
+          expected: [],
+        },
+        {
+          file: {
+            type: "css",
+            content: "/* \n @media screen { .foobar{color:blue;} }*/",
+          },
+          expected: [],
+        },
+        {
+          file: {
+            type: "css",
+            content: "/* \n @media screen { .foobar{color:red;} }*/" +
+              "@media screen { .foobar{color:blue;} }",
+          },
+          expected: [
+            {
+              content: " .foobar{color:blue;} ",
+              type: EMBED_TYPE_CSS,
+              startIndex: 59,
+              endIndex: 81,
+              getRaw(): string { return " .foobar{color:blue;} "; },
+            },
+          ],
+        },
+        {
+          file: {
+            type: "css",
+            content: "@media screen { .foobar{color:red;} }" +
+              "/* @media screen { .foobar{color:blue;} } */",
+          },
+          expected: [
+            {
+              content: " .foobar{color:red;} ",
+              type: EMBED_TYPE_CSS,
+              startIndex: 15,
+              endIndex: 36,
+              getRaw(): string { return " .foobar{color:red;} "; },
+            },
+          ],
+        },
+        {
+          file: {
+            type: "css",
+            content: "/* foo */@media screen { .foobar{color:red;} }/* bar */",
+          },
+          expected: [
+            {
+              content: " .foobar{color:red;} ",
+              type: EMBED_TYPE_CSS,
+              startIndex: 24,
+              endIndex: 45,
+              getRaw(): string { return " .foobar{color:red;} "; },
+            },
+          ],
+        },
+      ],
+    },
+    {
+      testName: "edge cases, with matches",
+      getScenario: () => [
+        {
+          file: {
+            type: "css",
+            content: "@media screen { }",
+          },
+          expected: [
+            {
+              content: " ",
+              type: EMBED_TYPE_CSS,
+              startIndex: 15,
+              endIndex: 16,
+              getRaw(): string { return " "; },
+            },
+          ],
+        },
+      ],
+    },
+    {
+      testName: "edge cases, without matches",
+      getScenario: () => [
+        {
+          file: {
+            type: "css",
+            content: ".foobar{color:purple;}",
+          },
+          expected: [],
+        },
+        {
+          file: {
+            type: "css",
+            content: "@media screen {}",
+          },
+          expected: [],
+        },
+        {
+          file: {
+            type: "css",
+            content: "@mediascreen { .foobar{color:purple;} }",
+          },
+          expected: [],
         },
       ],
     },
--- packages/language-css/src/embeds/css/media-queries.ts
+++ packages/language-css/src/embeds/css/media-queries.ts
@@ -1,7 +1,67 @@
 import type { WebManglerEmbed, WebManglerFile } from "@webmangler/types";
 
 /**
- * Extract all style attribute values in a HTML file as CSS embeds.
+ * A regular expression to find media queries in CSS.
+ */
+const REGEXP_MEDIA_QUERY = /(?:\/\*(?:.|\n|\r)*?\*\/|(?<q>@media\s+[^{]*{)(?<v>(?:{[^}]*}|[^}])+)})/gmi;
+
+/**
+ * Convert a {@link REGEXP_MEDIA_QUERY} match into a {@link WebManglerEmbed}.
+ *
+ * @param match A {@link RegExpExecArray}.
+ * @returns The {@link WebManglerEmbed}, or `null` if the match was improper.
+ */
+function matchToEmbed(
+  match: RegExpExecArray,
+): WebManglerEmbed | null {
+  const groups = match.groups as { [key: string]: string; };
+  const query = groups.q;
+  const stylesheet = groups.v;
+  if (stylesheet === undefined) {
+    return null;
+  }
+
+  const startIndex = match.index + query.length;
+  const endIndex = startIndex + stylesheet.length;
+
+  return {
+    type: "css",
+    startIndex,
+    endIndex,
+    content: stylesheet,
+    getRaw(): string {
+      return this.content;
+    },
+  };
+}
+
+/**
+ * Find all media query embeds in a file given a Regular Expression that
+ * matches media queries.
+ *
+ * @param file A {@link WebManglerFile}.
+ * @param regExp A {@link RegExp}.
+ * @returns Zero or more {@link WebManglerEmbed}s.
+ */
+function matchAndGetEmbeds(
+  file: WebManglerFile,
+  regExp: RegExp,
+): Iterable<WebManglerEmbed> {
+  const result: WebManglerEmbed[] = [];
+
+  let match: RegExpExecArray | null = null;
+  while ((match = regExp.exec(file.content)) !== null) {
+    const embed = matchToEmbed(match);
+    if (embed !== null) {
+      result.push(embed);
+    }
+  }
+
+  return result;
+}
+
+/**
+ * Extract all media queries in a CSS file as CSS embeds.
  *
  * @example
  * const file = { type: "css", content: "@media screen { .foobar{} }" };
@@ -14,7 +74,7 @@ function getMediaQueriesAsEmbeds(
   file: WebManglerFile,
 ): Iterable<WebManglerEmbed> {
   return [
-    // TODO: Add implementation
+    ...matchAndGetEmbeds(file, REGEXP_MEDIA_QUERY),
   ];
 }
 
