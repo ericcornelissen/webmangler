318f08c8d65cc54e87bce9979e66b426ceee3fbe
Eric Cornelissen <ericornelissen@gmail.com>
2022-07-05 00:01:31 +0200

    refactor(mangler-css-classes): make options fields `readonly`
    Update the `CssClassManglerOptions` so that all options are treated as
    `readonly` bye the `CssClassMangler`. This does not affect users of this
    package, it just provides them a guarentee that the object won't get new
    value references.
    
    The version update of `CssClassManglerOptions` is updated accordingly.
    Note the TSDoc changes for `ignoreClassNamePattern` include the addition
    of the since tag (based on the CHANGELOG.md for the core package, line
    31) that was missing before.

--- packages/mangler-css-classes/src/types.ts
+++ packages/mangler-css-classes/src/types.ts
@@ -2,7 +2,7 @@
  * The options for _WebMangler_'s built-in CSS class mangler.
  *
  * @since v0.1.0
- * @version v0.1.23
+ * @version v0.1.24
  */
 interface CssClassManglerOptions {
   /**
@@ -10,17 +10,18 @@ interface CssClassManglerOptions {
    *
    * @default "cls-[a-zA-Z-_]+"
    * @since v0.1.0
-   * @version v0.1.17
+   * @version v0.1.24
    */
-  classNamePattern?: string | Iterable<string>;
+  readonly classNamePattern?: string | Iterable<string>;
 
   /**
    * One or more patterns for CSS classes that should **never** be mangled.
    *
    * @default []
-   * @version v0.1.23
+   * @since v0.1.23
+   * @version v0.1.24
    */
-  ignoreClassNamePattern?: string | Iterable<string>;
+  readonly ignoreClassNamePattern?: string | Iterable<string>;
 
   /**
    * A list of strings and patterns of CSS class names that should not be used.
@@ -29,17 +30,18 @@ interface CssClassManglerOptions {
    *
    * @default []
    * @since v0.1.0
-   * @version v0.1.17
+   * @version v0.1.24
    */
-  reservedClassNames?: Iterable<string>;
+  readonly reservedClassNames?: Iterable<string>;
 
   /**
    * A prefix to use for mangled CSS classes.
    *
    * @default ""
    * @since v0.1.0
+   * @version v0.1.24
    */
-  keepClassNamePrefix?: string;
+  readonly keepClassNamePrefix?: string;
 
   /**
    * A list of HTML attributes whose value to treat as `class`.
@@ -49,9 +51,9 @@ interface CssClassManglerOptions {
    *
    * @default ["class"]
    * @since v0.1.16
-   * @version v0.1.17
+   * @version v0.1.24
    */
-  classAttributes?: Iterable<string>;
+  readonly classAttributes?: Iterable<string>;
 }
 
 export type {
