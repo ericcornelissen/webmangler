2a81ffe45f6232d51fb3b7ecb49d2f9405179b09
Eric Cornelissen <ericornelissen@gmail.com>
2022-08-22 22:44:34 +0200

    refactor(language-css): write common regexps as literals
    Update the common regular expressions for the language-css plugin to be
    written as literals, converting them back to strings using the
    `.source` [1] and improvised interpolation using `.replace` (the String
    prototype method).
    
    Having these regular expressions as literals enable static analysis
    tools (currently at least ESLint and CodeQL) to process them and flag
    potential problems.
    
    --
    1. https://developer.mozilla.org/en-US/docs/Web/JavaScript/
       Reference/Global_Objects/RegExp/source

--- packages/language-css/src/expressions/common.ts
+++ packages/language-css/src/expressions/common.ts
@@ -7,70 +7,76 @@ const QUOTES_ARRAY = ["\"", "'"];
  * Regular Expression pattern as a string of ways to combine query selectors in
  * CSS.
  */
-const querySelectorCombiners = "(?:\\s|\\,|\\>|\\+|\\~)";
+const querySelectorCombiners = /(?:\s|,|>|\+|~)/.source;
 
 /**
  * Regular Expression pattern as a string of characters that may appear after
  * the end of a query selector in CSS.
  */
-const allowedAfterSelector =
-  `(?:${querySelectorCombiners}|\\.|\\#|\\[|\\:|\\))`;
+const allowedAfterSelector = /(?:${querySelectorCombiners}|\.|#|\[|:|\))/.source
+  .replace("${querySelectorCombiners}", querySelectorCombiners);
 
 /**
  * Regular Expression pattern as a string of characters that may appear before
  * the start of a query selector in CSS.
  */
-const allowedBeforeSelector = `(?:${querySelectorCombiners}|\\()`;
+const allowedBeforeSelector = /(?:${querySelectorCombiners}|\()/.source
+  .replace("${querySelectorCombiners}", querySelectorCombiners);
 
 /**
  * Regular Expression pattern as a string of all arithmetic operators in CSS.
  */
-const arithmeticOperators = "(?:\\+|\\-|\\*|\\/)";
+const arithmeticOperators = /(?:\+|-|\*|\/)/.source;
 
 /**
  * Regular Expression pattern as a string of  all attribute operators in CSS.
  */
-const attributeOperators = "(?:\\=|\\~=|\\|=|\\^=|\\$=|\\*=)";
+const attributeOperators = /(?:=|~=|\|=|\^=|\$=|\*=)/.source;
 
 /**
  * Regular Expression pattern as a string of a comment opening in CSS.
  */
-const commentOpen = "(?:\\/\\*)";
+const commentOpen = /(?:\/\*)/.source;
 
 /**
  * Regular Expression pattern as a string of a comment closing in CSS.
  */
-const commentClose = "(?:\\*\\/)";
+const commentClose = /(?:\*\/)/.source;
 
 /**
  * Regular Expression pattern as a string of a comment in CSS.
  */
-const comment = `(?:${commentOpen}(?:.|\\n|\\r)*?${commentClose})`;
+const comment = /(?:${commentOpen}(?:.|\n|\r)*?${commentClose})/.source
+  .replace("${commentOpen}", commentOpen)
+  .replace("${commentClose}", commentClose);
 
 /**
  * Regular Expression pattern as a string for a double quoted string in CSS.
  */
-const doubleQuotedString = "(?:\"(?:\\\\\"|[^\"])*\")";
+const doubleQuotedString = /(?:"(?:\\"|[^"])*")/.source;
 
 /**
  * Regular Expression pattern as a string for a single quoted string in CSS.
  */
-const singleQuotedString = "(?:'(?:\\\\'|[^'])*')";
+const singleQuotedString = /(?:'(?:\\'|[^'])*')/.source;
 
 /**
  * Regular Expression pattern as a string for a string in CSS.
  */
-const anyString = `(?:${doubleQuotedString}|${singleQuotedString})`;
+const anyString = /(?:${doubleQuotedString}|${singleQuotedString})/.source
+  .replace("${doubleQuotedString}", doubleQuotedString)
+  .replace("${singleQuotedString}", singleQuotedString);
 
 /**
  * Regular Expression pattern as a string for quotes in CSS.
  */
-const quotes = "(?:\"|')";
+const quotes = /(?:"|')/.source;
 
 /**
  * Regular Expression pattern as a string for a CSS ruleset.
  */
-const ruleset = `(?:\\{(?:${anyString}|[^}'"])*?\\})`;
+const ruleset = /(?:\{(?:${anyString}|[^}'"])*?\})/.source
+  .replace("${anyString}", anyString);
 
 /**
  * An object of common Regular Expression patterns in CSS.
