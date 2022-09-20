c21a004e059773059b085318ba8aca0d436aefea
Eric Cornelissen <ericornelissen@gmail.com>
2022-09-20 23:43:19 +0200

    feat(language-css): add `kind`-based implementation for query selectors
    Update the query selectors expression factory to work based purely on
    the `kind` option of the `QuerySelectorOptions`.
    
    The implementation is based entirely off of the pre-existing
    implementation, simply delegating calls to the (pre-existing)
    `newCssSelectorExpression` function using prefix and suffix values that
    come from the mangler plugins (see `packages/mangler*/src/helpers/
    language-options.ts`).

--- packages/language-css/src/expressions/query-selectors.ts
+++ packages/language-css/src/expressions/query-selectors.ts
@@ -70,6 +70,60 @@ function fallback(
 }
 
 /**
+ * Get {@link MangleExpression}s to match query selectors for attributes in CSS,
+ * e.g. `data-foobar` in `[data-foobar] { }`.
+ *
+ * @returns The {@link MangleExpression}s.
+ */
+function newAttributeSelectorExpression(): Iterable<MangleExpression> {
+  return newCssSelectorExpression({
+    kind: "attribute",
+    prefix: /\[\s*/.source,
+    suffix: /\s*(?:]|${attributeOperators})/.source
+      .replace("${attributeOperators}", patterns.attributeOperators),
+  });
+}
+
+/**
+ * Get {@link MangleExpression}s to match query selectors for classes in CSS,
+ * e.g. `foobar` in `.foobar { }`.
+ *
+ * @returns The {@link MangleExpression}s.
+ */
+function newClassSelectorExpression(): Iterable<MangleExpression> {
+  return newCssSelectorExpression({
+    kind: "class",
+    prefix: /\./.source,
+  });
+}
+
+/**
+ * Get {@link MangleExpression}s to match query selectors for elements in CSS,
+ * e.g. `div` in `div { }`.
+ *
+ * @returns The {@link MangleExpression}s.
+ */
+function newElementSelectorExpression(): Iterable<MangleExpression> {
+  return newCssSelectorExpression({
+    kind: "element",
+  });
+}
+
+
+/**
+ * Get {@link MangleExpression}s to match query selectors for IDs in CSS, e.g.
+ * `foobar` in `#foobar { }`.
+ *
+ * @returns The {@link MangleExpression}s.
+ */
+function newIdSelectorExpression(): Iterable<MangleExpression> {
+  return newCssSelectorExpression({
+    kind: "id",
+    prefix: /#/.source,
+  });
+}
+
+/**
  * Get the set of {@link MangleExpression}s to match CSS selectors in CSS. This
  * will match:
  * - Attribute selectors (e.g. `foobar` in `[foobar] { }`).
@@ -91,7 +145,10 @@ function querySelectorExpressionFactory(
   }
 
   return [
-    // TODO: Add implementation
+    ...(options.kind === "attribute" ? newAttributeSelectorExpression() : []),
+    ...(options.kind === "class" ? newClassSelectorExpression() : []),
+    ...(options.kind === "element" ? newElementSelectorExpression() : []),
+    ...(options.kind === "id" ? newIdSelectorExpression() : []),
   ];
 }
 
